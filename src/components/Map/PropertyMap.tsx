9df8cb35b00b83975671e3fd88836e999f1d7b93
Jiwoo Park <jiwoopark727@gmail.com>
2024-10-31 18:47:20 +0900

    design: 마커 사이즈 조절

--- src/components/Map/PropertyMap.tsx
+++ src/components/Map/PropertyMap.tsx
@@ -16,7 +16,7 @@ const PropertyMap = () => {
       const map = new window.kakao.maps.Map(container, options);
 
       const imageSrc = 'src/assets/images/marker.png', // 마커이미지의 주소입니다
-        imageSize = new window.kakao.maps.Size(60, 59), // 마커이미지의 크기입니다
+        imageSize = new window.kakao.maps.Size(45, 45), // 마커이미지의 크기입니다
         imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
       // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
